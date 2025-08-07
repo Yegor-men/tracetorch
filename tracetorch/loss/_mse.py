83b5131475baf3daa5f85910cb49e97e6960bea7
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-07 16:50:39 +1000

    Loss LS calculations.
    MSE now defaults to sum, thus directly showing how many neurons are wrong, just more comfortable to inspect.

--- tracetorch/loss/_mse.py
+++ tracetorch/loss/_mse.py
@@ -1,11 +1,15 @@
 import torch
 
 
-def mse(received: torch.Tensor, expected: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+def mse(
+		received: torch.Tensor,
+		expected: torch.Tensor,
+		reduction: str = "sum"
+) -> tuple[torch.Tensor, torch.Tensor]:
 	received = received.detach().clone().requires_grad_(True)
 	expected = expected.detach().to(received)
 
-	loss_fn = torch.nn.MSELoss()
+	loss_fn = torch.nn.MSELoss(reduction=reduction)
 	loss = loss_fn.forward(received, expected)
 	loss.backward()
 
