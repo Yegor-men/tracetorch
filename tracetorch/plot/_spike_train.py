71b059ec9df348a0d02226d135f61b469d528753
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-08 12:04:24 +1000

    Added title to plot.

--- tracetorch/plot/_spike_train.py
+++ tracetorch/plot/_spike_train.py
@@ -6,7 +6,9 @@ import numpy as np
 def spike_train(list_of_tensors,
 				spacing: float = 1.0,
 				linelength: float = 0.8,
-				linewidth: float = 0.5):
+				linewidth: float = 0.5,
+				title: str = "Spike Train Raster"
+				) -> None:
 	N = list_of_tensors[0].numel()
 
 	# for each neuron i, collect all t where vec[i] != 0
@@ -27,5 +29,5 @@ def spike_train(list_of_tensors,
 
 	plt.xlabel("Time step")
 	plt.ylabel("Neuron index")
-	plt.title("Spike Train Raster")
+	plt.title(title)
 	plt.show()
