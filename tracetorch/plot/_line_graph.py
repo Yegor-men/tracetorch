5d20142f9834c4ff88daa1a8a8a6178a3a5f0702
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-08 16:14:34 +1000

    List no longer has to be tensors, can just be floats.

--- tracetorch/plot/_line_graph.py
+++ tracetorch/plot/_line_graph.py
@@ -2,24 +2,31 @@ import torch
 import matplotlib.pyplot as plt
 
 
-def line_graph(list_of_tensors: list[torch.Tensor], title: str) -> None:
-	T = len(list_of_tensors)
-	N = list_of_tensors[0].numel()
+def line_graph(list_of_values, title: str) -> None:
+	# Check if it's a list of tensors
+	if isinstance(list_of_values[0], torch.Tensor):
+		T = len(list_of_values)
+		N = list_of_values[0].numel()
 
-	# stack into shape (T, …)
-	data = torch.stack(list_of_tensors, dim=0)  # if scalars, shape == (T,)
-	# quick fix: if it came out 1-D, treat as (T,1)
-	if data.dim() == 1:
-		data = data.unsqueeze(1)  # now shape == (T,1)
+		# stack into shape (T, …)
+		data = torch.stack(list_of_values, dim=0)  # if scalars, shape == (T,)
+		if data.dim() == 1:
+			data = data.unsqueeze(1)  # now shape == (T,1)
 
-	data = data.cpu().numpy()
-	x = range(T)
+		data = data.cpu().numpy()
+		x = range(T)
 
-	for neuron_idx in range(N):
-		plt.plot(x, data[:, neuron_idx], label=f'Neuron {neuron_idx}')
+		for neuron_idx in range(N):
+			plt.plot(x, data[:, neuron_idx], label=f'Neuron {neuron_idx}')
 
-	plt.title(title)
-	if N > 1:
-		plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+		plt.title(title)
+		if N > 1:
+			plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+	else:
+		# Assume list of floats (or ints)
+		x = range(len(list_of_values))
+		plt.plot(x, list_of_values)
+		plt.title(title)
+
 	plt.tight_layout()
 	plt.show()
