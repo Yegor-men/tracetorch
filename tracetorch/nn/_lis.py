8041156a7538a6b46ddff6720a2abbb7e9e0482f
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-13 21:51:51 +1000

    Fixed LIF and LIS setting of learnable parameters, improved .grad clearance to wipe all parameters.

--- tracetorch/nn/_lis.py
+++ tracetorch/nn/_lis.py
@@ -8,6 +8,7 @@ class LIS:
 	"""
 	Leaky integrate and softmax layer
 	"""
+
 	def __init__(
 			self,
 			num_in: int,
@@ -27,18 +28,34 @@ class LIS:
 		self.weight = (torch.randn(num_out, num_in) * weight_scaling).to(self.device)
 		self.mem_decay = (functional.sigmoid_inverse(torch.ones(num_out) * mem_decay)).to(self.device)
 		self.in_trace_decay = (functional.sigmoid_inverse(torch.ones(num_in) * in_trace_decay)).to(self.device)
-
-		self.weight.requires_grad_(learn_weight)
-		self.mem_decay.requires_grad_(learn_mem_decay)
-		self.in_trace_decay.requires_grad_(learn_in_trace_decay)
-
-		self.optimizer = torch.optim.AdamW(
-			[p for p in (self.weight, self.mem_decay, self.in_trace_decay) if p.requires_grad],
-			lr=self.lr
-		)
-
 		self.mem = torch.zeros(num_out).to(self.device)
 		self.in_trace = torch.zeros(num_in).to(self.device)
+
+		self.all_tensors = [
+			self.weight,
+			self.mem_decay,
+			self.in_trace_decay,
+			self.mem,
+			self.in_trace
+		]
+
+		for tensor in [self.weight, self.mem_decay, self.in_trace_decay]:
+			tensor.requires_grad_(True)
+
+		self.learnable_parameters = [
+			t for t, learn in [
+				(self.weight, learn_weight),
+				(self.mem_decay, learn_mem_decay),
+				(self.in_trace_decay, learn_in_trace_decay)
+			] if learn
+		]
+
+		self.optimizer = torch.optim.AdamW(self.learnable_parameters, lr=self.lr)
+
+	def clear_grads(self):
+		for t in self.all_tensors:
+			if t.grad is not None:
+				t.grad = None
 
 	def forward(self, in_spikes: torch.Tensor) -> torch.Tensor:
 		with torch.no_grad():
@@ -68,7 +85,7 @@ class LIS:
 		average_input.backward(passed_ls)
 
 		self.optimizer.step()
-		self.optimizer.zero_grad(set_to_none=True)
+		self.clear_grads()
 		return passed_ls
 
 	def zero_states(self):
