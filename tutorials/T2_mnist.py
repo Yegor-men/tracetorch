2db0c7cf333d9ba3aa147d0bc629051fa90ecc51
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-15 22:48:01 +1000

    Added plotting of the confusion matrix

--- tutorials/T2_mnist.py
+++ tutorials/T2_mnist.py
@@ -10,8 +10,8 @@ torch.cuda.manual_seed_all(0)
 # device = "cuda" if torch.cuda.is_available() else "cpu"
 device = "cpu"
 
-min_prob = 0.05
-max_prob = 1
+min_prob = 0.1
+max_prob = 0.5
 
 # Flattens the image and redistributes the brightness domain from [0, 1] to [min_prob, max_prob]
 image_transform = transforms.Compose([
@@ -40,7 +40,7 @@ test_dataset = datasets.MNIST(train=False, **dataset_kwargs)
 train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
 test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
 
-n_hidden = 8
+n_hidden = 10
 
 model = snn.Sequential(
 	snn.LIF(
@@ -70,7 +70,6 @@ optimizer = torch.optim.Adam(params=model.get_learnable_parameters(), lr=1e-4)
 for epoch in range(num_epochs):
 	for index, (x, y) in tqdm(enumerate(train_dataloader), total=len(train_dataloader), leave=True, desc=f"E{epoch}"):
 		x, y = x.to(device), y.to(device)
-		model.zero_grad(set_to_none=True)
 		model.zero_states()
 		aggregate = torch.zeros(model.num_out).to(device)
 		for _ in range(think_steps):
@@ -81,6 +80,7 @@ for epoch in range(num_epochs):
 		loss, ls = tt.loss.mse(aggregate, y)
 		model.backward(ls)
 		optimizer.step()
+		model.zero_grad(set_to_none=True)
 		loss_manager.append(loss)
 		accuracy_manager.append(1 if aggregate.argmax().item() == y.argmax().item() else 0)
 		if (index % 10000 == 0) and (index != 0):
@@ -90,21 +90,42 @@ for epoch in range(num_epochs):
 loss_manager.plot()
 accuracy_manager.plot()
 
-net_loss = torch.zeros_like(loss)
+net_loss = torch.tensor(0).to(device)
 net_accuracy = 0
+mistakes_matrix = torch.zeros(10, 10)
 
 for index, (x, y) in tqdm(enumerate(test_dataloader), total=len(test_dataloader), leave=True, desc=f"Testing"):
+	x, y = x.to(device), y.to(device)
 	model.zero_states()
+	aggregate = torch.zeros(model.num_out).to(device)
+	raw_image = torch.zeros_like(x)
 	for _ in range(think_steps):
-		model_dist = model.forward(torch.bernoulli(x).to(device))
-	loss, ls = tt.loss.mse(model_dist, y.to(device))
+		bern = torch.bernoulli(x)
+		raw_image += bern
+		model_dist = model.forward(bern)
+		aggregate += model_dist
+	aggregate /= aggregate.sum()
+	raw_image /= think_steps
+	if index % 1000 == 0:
+		tt.plot.render_image(raw_image.unsqueeze(0).view(28, 28))
+	loss, ls = tt.loss.mse(aggregate, y)
 	net_loss += loss
-	net_accuracy += (1 if model_dist.argmax().item() == y.argmax().item() else 0)
+	chosen_index = int(aggregate.argmax().item())
+	real_index = int(y.argmax().item())
+	correct = chosen_index == real_index
+	if not correct:
+		mistakes_matrix[real_index][chosen_index] += 1
+	else:
+		net_accuracy += 1
 
 net_loss /= len(test_dataloader)
 net_accuracy /= len(test_dataloader)
 
 print(f"\tTEST\nLoss: {net_loss}\nAccuracy: {net_accuracy}")
+for index, mistakes in enumerate(mistakes_matrix):
+	print(f"Index: {index}, n_mistakes: {mistakes}")
+
+tt.plot.render_image(mistakes_matrix)
 
 in_trace_decays = [torch.nn.functional.sigmoid(layer.in_trace_decay) for layer in model.layers]
 mem_decays = [torch.nn.functional.sigmoid(layer.mem_decay) for layer in model.layers]
@@ -115,25 +136,3 @@ tt.plot.distributions(in_trace_decays, title="Input trace decay")
 tt.plot.distributions(mem_decays, title="Membrane decay")
 tt.plot.distributions(weights, title="Weights")
 tt.plot.distributions(thresholds, title="Thresholds")
-
-test_samples = 32
-test_iter = iter(test_dataloader)
-
-for i in range(test_samples):
-	(x, y) = next(test_iter)
-	tt.plot.render_image(x.unsqueeze(0).view(28, 28))
-	x, y = x.to(device), y.to(device)
-	model.zero_states()
-	aggregate = torch.zeros(model.num_out).to(device)
-	out_dists = []
-	for j in range(think_steps):
-		bern = torch.bernoulli(x).to(device)
-		model_dist = model.forward(bern)
-		aggregate += model_dist
-		out_dists.append(model_dist)
-	aggregate /= aggregate.sum()
-	loss, ls = tt.loss.mse(aggregate, y)
-	chosen_index = int(aggregate.argmax().item())
-	real_index = int(y.argmax().item())
-	correct = chosen_index == real_index
-	tt.plot.spike_train(out_dists, title=f"Loss: {loss}, {correct}, {chosen_index}, {real_index}")
