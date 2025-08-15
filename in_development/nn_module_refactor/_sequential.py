b8ba7f2a2b002e5946eb78446ed6712054707a53
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-15 12:11:20 +1000

    Created refactored Sequential class, centred around nn.Module

---
+++ in_development/nn_module_refactor/_sequential.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+
+
+class Sequential(nn.Module):
+	def __init__(self, *layers):
+		super().__init__()
+		self.layers = nn.ModuleList(layers)
+
+	def forward(self, x: torch.Tensor) -> torch.Tensor:
+		for layer in self.layers:
+			x = layer.forward(x)
+		return x
+
+	def backward(self, ls: torch.Tensor) -> None:
+		for layer in reversed(self.layers):
+			ls = layer.backward(ls)
+		return ls
+
+	def zero_states(self):
+		for layer in self.layers:
+			layer.zero_states()
+
+	def clear_gradients(self):
+		for param in self.parameters():
+			param.grad = None
