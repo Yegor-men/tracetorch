Fixed REFLECT learning signal calculation
In normal RL, the learning signal is the derivative of the loss w.r.t. the probability, given that the loss is `-advantage * sum(choice * log(probability))`, it's quite simple since the entire computation graph already has everything to get `d_L/d_probability`, but in the case of REFLECT, we are storing the trace of the logprobs, and the raw probabilities themselves aren't used in the computation graph. Therefore the best we can return is average_logprob.grad, but LIS expected `d_L/d_probability`. Subsequently, the solution is to just adjust LIS so that it _can_ expect `d_L/d_logprob`, since applying log to the probability inside the LIS .backward() is easy as, and then on that is called .backward(-ls). The learning signal is negative because log returns the opposite of what you want, a signal saying "decrease" really means the opposite.

tracetorch/loss/_reflect.py
@@ -54,8 +54,8 @@ class Reflect(nn.Module):
 		advantage = reward - baseline_reward
 
 		average_logprob = self.logprob_trace * (1 - d)
-		average_logprob.retain_grad()
 		average_output = self.output_trace * (1 - d)
+		average_logprob.retain_grad()
 
 		loss = -advantage * (average_output * average_logprob).sum()
 

tracetorch/snn/_lis.py
@@ -14,10 +14,12 @@ class LIS(nn.Module):
 			learn_weight: bool = True,
 			learn_mem_decay: bool = True,
 			learn_in_trace_decay: bool = True,
+			use_logprob_backward: bool = False,
 	):
 		super().__init__()
 		self.num_in = num_in
 		self.num_out = num_out
+		self.use_logprob_backward = bool(use_logprob_backward)
 
 		self.learn_weight = learn_weight
 		self.learn_mem_decay = learn_mem_decay
@@ -64,10 +66,14 @@ class LIS(nn.Module):
 		i = torch.einsum("i, oi -> o", average_input, self.weight)
 		d = nn.functional.sigmoid(self.mem_decay)
 
-		stabilized_mem_level = i / (1 - d + 1e-6)
+		stabilized_mem_level = i / (1 - d + 1e-12)
 		frequency = torch.nn.functional.softmax(stabilized_mem_level, dim=-1)
+		if self.use_logprob_backward:
+			logprob = torch.log(frequency + 1e-12)
+			logprob.backward(-learning_signal.detach())
+		else:
+			frequency.backward(learning_signal.detach())
 
-		frequency.backward(learning_signal.detach())
 		passed_learning_signal = average_input.grad.detach().clone()
 		average_input.grad = None
 		del average_input

tutorials/T2_RL_mnist.py
@@ -10,8 +10,8 @@ torch.cuda.manual_seed_all(0)
 # device = "cuda" if torch.cuda.is_available() else "cpu"
 device = "cpu"
 
-min_prob = 0.1
-max_prob = 0.5
+min_prob = 0
+max_prob = 1
 
 # Flattens the image and redistributes the brightness domain from [0, 1] to [min_prob, max_prob]
 image_transform = transforms.Compose([
@@ -54,6 +54,7 @@ model = snn.Sequential(
 	snn.LIS(
 		num_in=n_hidden,
 		num_out=10,
+		use_logprob_backward=True
 	)
 ).to(device)
 