5961d0c7f62209f38b6bf0e52bb1bbdfd2985b77
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-21 17:38:59 +1000

    Added sigmoid derivative calculator for e-prop surrogate derivative

--- tracetorch/functional/__init__.py
+++ tracetorch/functional/__init__.py
@@ -2,3 +2,4 @@ from ._sigmoid_inverse import sigmoid_inverse
 from ._softplus_inverse import softplus_inverse
 from ._sample_softmax import sample_softmax
 from ._halflife_decay_conversion import halflife_to_decay, decay_to_halflife
+from ._sigmoid_derivative import sigmoid_derivative
