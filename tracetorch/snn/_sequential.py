5614b84fe1099e963e9184fe3887d5653b8d650b
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-22 17:52:27 +1000

    using gradients as a trace is... a concept for later. With the current implementation of traceTorch it's way too slow. Need to refactor to e-prop (in the process) in order for it to be even viable to test

--- tracetorch/snn/_sequential.py
+++ tracetorch/snn/_sequential.py
@@ -9,22 +9,6 @@ class Sequential(nn.Module):
 		self.num_in = int(self.layers[0].num_in)
 		self.num_out = int(self.layers[-1].num_out)
 
-	@torch.no_grad()
-	def decay_grad(self, decay):
-		for p in self.parameters():
-			if p is not None:
-				p.mul_(decay)
-
-	@torch.no_grad()
-	def optim_trace_step(self, optimizer: torch.optim, decay):
-		clones = [p.grad.detach().clone() if p is not None else None for p in self.parameters()]
-		for p in self.parameters():
-			p.grad.mul_(1 - decay)
-		optimizer.step()
-		for p, c in zip(self.parameters(), clones):
-			p.grad.copy_(c)
-		del clones
-
 	def forward(self, x: torch.Tensor) -> torch.Tensor:
 		for layer in self.layers:
 			x = layer.forward(x)
