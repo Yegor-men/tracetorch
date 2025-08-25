b87f63f26a6914abc83804c28b13d3b669a937a4
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-25 17:01:56 +1000

    Works in conjunction with new ALIF and LIS

--- tracetorch/eprop/_sequential.py
+++ tracetorch/eprop/_sequential.py
@@ -9,6 +9,14 @@ class Sequential(nn.Module):
 		self.num_in = int(self.layers[0].num_in)
 		self.num_out = int(self.layers[-1].num_out)
 
+	def zero_states(self):
+		for layer in self.layers:
+			layer.zero_states()
+
+	def zero_elig(self):
+		for layer in self.layers:
+			layer.zero_elig()
+
 	def forward(self, x: torch.Tensor) -> torch.Tensor:
 		for layer in self.layers:
 			x = layer.forward(x)
@@ -19,9 +27,5 @@ class Sequential(nn.Module):
 			ls = layer.backward(ls)
 
 	def elig_to_grad(self, scalar: float = 1.0):
-		for layer in self.layers[:-1]:
-			layer.elig_to_grad(scalar)
-
-	def zero_states(self):
 		for layer in self.layers:
-			layer.zero_states()
+			layer.elig_to_grad(scalar)
