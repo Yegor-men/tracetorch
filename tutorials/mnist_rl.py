4ae43f31a55697730b72f5ae0bac0ac4d24e6631
Yegor Men <yegormenovchshikov@gmail.com>
2025-08-26 11:57:24 +1000

    Created sparse RL reward training, seems to struggle in the exact same way as the old approach? weird

--- tutorials/mnist_rl.py
+++ tutorials/mnist_rl.py
@@ -54,62 +54,51 @@ model = snn.Sequential(
 	snn.LIS(
 		num_in=n_hidden,
 		num_out=10,
-		expect_logprob_ls=True
 	)
 ).to(device)
 
-REFLECT = tt.loss.Reflect(num_in=model.num_out, decay=0.9)
+model_optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-4)
 
-model_optimizer = torch.optim.AdamW(params=model.get_learnable_parameters(), lr=1e-4)
-reflect_optimizer = torch.optim.AdamW(params=REFLECT.get_learnable_parameters(), lr=1e-3)
-
-loss_manager = tt.plot.MeasurementManager(title="Loss")
 accuracy_manager = tt.plot.MeasurementManager(title="Accuracy")
-reflect_manager = tt.plot.MeasurementManager(title="REFLECT decay", decay=[0.0])
 
 from tqdm import tqdm
 
-think_steps = 20
+think_steps = 10
 num_epochs = 1
+
+rolling_reward = 0
+reward_decay = 0.9
 
 for epoch in range(num_epochs):
 	for index, (x, y) in tqdm(enumerate(train_dataloader), total=len(train_dataloader), leave=True, desc=f"E{epoch}"):
 		x, y = x.to(device), y.to(device)
 		model.zero_states()
-		REFLECT.zero_states(clear_reward=False)
 
 		aggregate = torch.zeros(model.num_out).to(device)
 		for _ in range(think_steps):
 			bern = torch.bernoulli(x)
 			model_dist = model.forward(bern)
 			aggregate += model_dist
-			model_output = tt.functional.sample_softmax(model_dist)
-			REFLECT.forward(model_dist, model_output)
+			sample = tt.functional.sample_softmax(model_dist)
+			loss, ls = tt.loss.logprob(model_dist, sample)
+			model.backward(ls)
+
 		aggregate /= think_steps
 		reward = 1 if aggregate.argmax().item() == y.argmax().item() else 0
+		rolling_reward = rolling_reward * reward_decay + reward
+		average_reward = rolling_reward * (1 - reward_decay)
+		advantage = reward - average_reward
 
-		loss, ls = REFLECT.backward(reward)
-		# print(f"Average Distribution: {average_distribution}")
-		# print(f"Passed LS max: {ls.abs().max()}")
-		model.backward(ls)
-
+		model.elig_to_grad(advantage)
 		model_optimizer.step()
-		# reflect_optimizer.step()
-		model.zero_grad(set_to_none=True)
-		REFLECT.zero_grad(set_to_none=True)
+		model.zero_grad()
 
-		loss_manager.append(loss)
 		accuracy_manager.append(reward)
-		reflect_manager.append(torch.nn.functional.sigmoid(REFLECT.decay))
 
 		if (index % 10000 == 0) and (index != 0):
-			loss_manager.plot()
 			accuracy_manager.plot()
-			reflect_manager.plot()
 
-loss_manager.plot()
 accuracy_manager.plot()
-reflect_manager.plot()
 
 net_loss = torch.tensor(0.).to(device)
 net_accuracy = 0
@@ -118,6 +107,7 @@ mistakes_matrix = torch.zeros(10, 10)
 for index, (x, y) in tqdm(enumerate(test_dataloader), total=len(test_dataloader), leave=True, desc=f"Testing"):
 	x, y = x.to(device), y.to(device)
 	model.zero_states()
+
 	aggregate = torch.zeros(model.num_out).to(device)
 	raw_image = torch.zeros_like(x)
 	model_dists = []
@@ -129,9 +119,11 @@ for index, (x, y) in tqdm(enumerate(test_dataloader), total=len(test_dataloader)
 		model_dists.append(model_dist)
 	aggregate /= think_steps
 	raw_image /= think_steps
+
 	if index % 1000 == 0:
 		tt.plot.render_image(raw_image.unsqueeze(0).view(28, 28))
 		tt.plot.spike_train(model_dists, title="Model outputs")
+
 	loss, ls = tt.loss.mse(aggregate, y)
 	net_loss += loss
 	chosen_index = int(aggregate.argmax().item())
@@ -149,14 +141,14 @@ print(f"\tTEST\nLoss: {net_loss}\nAccuracy: {net_accuracy}")
 for index, mistakes in enumerate(mistakes_matrix):
 	print(f"Index: {index} confused for: {mistakes}")
 
-in_trace_decays = [torch.nn.functional.sigmoid(layer.in_trace_decay) for layer in model.layers]
-mem_decays = [torch.nn.functional.sigmoid(layer.mem_decay) for layer in model.layers]
-weights = [layer.weight for layer in model.layers]
-thresholds = [torch.nn.functional.softplus(layer.threshold) for layer in model.layers[:-1]]
-
-tt.plot.distributions(in_trace_decays, title="Input trace decay")
-tt.plot.distributions(mem_decays, title="Membrane decay")
-tt.plot.distributions(weights, title="Weights")
-tt.plot.distributions(thresholds, title="Thresholds")
-
-tt.plot.render_image(mistakes_matrix)
+# in_trace_decays = [torch.nn.functional.sigmoid(layer.in_trace_decay) for layer in model.layers]
+# mem_decays = [torch.nn.functional.sigmoid(layer.mem_decay) for layer in model.layers]
+# weights = [layer.weight for layer in model.layers]
+# thresholds = [torch.nn.functional.softplus(layer.threshold) for layer in model.layers[:-1]]
+#
+# tt.plot.distributions(in_trace_decays, title="Input trace decay")
+# tt.plot.distributions(mem_decays, title="Membrane decay")
+# tt.plot.distributions(weights, title="Weights")
+# tt.plot.distributions(thresholds, title="Thresholds")
+#
+# tt.plot.render_image(mistakes_matrix)
