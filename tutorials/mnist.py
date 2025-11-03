75bf2e5e963df5732f59e7abfa5d6c208bf2b9b8
Yegor Men <yegormenovchshikov@gmail.com>
2025-11-03 15:07:50 +1100

    Made MNIST significantly more difficult, the model actually needs to learn. The initial beta decay is set to a completely meaningless value that makes the model effectively zero gradient flow through time and the model must learn to make the value bigger, which it does, even with supervised delayed which is a little weird considering that it's basically training exclusively off the last timestep? Min max and noise values can be fiddled with, but right now it's hard enough.

--- tutorials/mnist.py
+++ tutorials/mnist.py
@@ -26,7 +26,17 @@ Here's what actually happens for the losses and the autograd graphs for their co
 
 Methods 1 and 3 are for when we explicitly want each at timestep to produce the correct output. Methods 2 and 4 are for
 when we don't particularly care about the intermediate outputs (or don't know what it should be), only what the last one
-must be.
+must be. The decay value for 1 and 3 is set so that the halflife is half the number of steps.
+
+We also intentionally make the task more difficult by having a baseline noise to the images, which makes all pixels more
+likely to fire, no matter their original color; and we also lower the max brightness so that the brightest pixels don't
+have a 100% chance of firing. The data is thus made to be much noisier; the brightest pixels should fire on average only
+once throughout the entire spike train. We also set the initial beta value for the model (membrane decay) intentionally
+low so that the model will lose information; if beta was high like 0.999, with minimal effort the model would just be
+able to cheat and accumulate the charge throughout the spike train; the model must learn to accumulate charge.
+
+The rendered images may be hard to identify; that is the intended effect, that is what the model optimally sees assuming
+it had learnt to maximally accumulate charge.
 """
 
 # ======================================================================================================================
@@ -42,7 +52,6 @@ from torchvision import datasets, transforms
 
 import copy
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 
 torch.manual_seed(0)
 torch.cuda.manual_seed_all(0)
@@ -57,7 +66,7 @@ def one_hot_encode(label):
 
 
 class OneHotMNIST(torch.utils.data.Dataset):
-	def __init__(self, train=True, min_val=0.0, max_val=0.5, offset=0.05):
+	def __init__(self, train=True, min_val=0.0, max_val=1.0, offset=0.0):
 		self.dataset = datasets.MNIST(
 			root='data',
 			train=train,
@@ -77,8 +86,22 @@ class OneHotMNIST(torch.utils.data.Dataset):
 		return len(self.dataset)
 
 
-train_dataset = OneHotMNIST(train=True, min_val=0.0, max_val=1.0, offset=0.00)
-test_dataset = OneHotMNIST(train=False, min_val=0.0, max_val=1.0, offset=0.00)
+# ======================================================================================================================
+
+num_epochs = 3
+num_timesteps = 20
+ema_decay = tt.functional.halflife_to_decay(num_timesteps / 2)
+model_layers_beta = tt.functional.halflife_to_decay(num_timesteps / 100)
+print(f"Original Decay: {model_layers_beta}")
+
+noise_offset = 0.1
+min_prob = 0.0
+max_prob = (1.0 / num_timesteps)
+
+# min_prob, max_prob, noise_offset = 0.0, 0.05, 0.01
+
+train_dataset = OneHotMNIST(train=True, min_val=min_prob, max_val=max_prob, offset=noise_offset)
+test_dataset = OneHotMNIST(train=False, min_val=min_prob, max_val=max_prob, offset=noise_offset)
 
 batch_size = 32
 num_to_visualize = 5
@@ -89,20 +112,19 @@ test_dloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 # ======================================================================================================================
 
 
-beta = 0.9
 model_template = snn.Sequential(
 	nn.Conv2d(1, 8, 5, dilation=2),
 	# kernel 5x5 @ dilation 2 becomes 9x9, 28-9+1=20
-	snn.Leaky(8, beta, 1.0, view_tuple=(1, -1, 1, 1)),
+	snn.Leaky(8, model_layers_beta, 1.0, view_tuple=(1, -1, 1, 1)),
 	nn.Conv2d(8, 16, 5, dilation=2),
 	# kernel 5x5 @ dilation 2 becomes 9x9, 20-9+1 = 12
-	snn.Leaky(16, beta, 1.0, view_tuple=(1, -1, 1, 1)),
+	snn.Leaky(16, model_layers_beta, 1.0, view_tuple=(1, -1, 1, 1)),
 	nn.Conv2d(16, 16, 5, dilation=2),
 	# kernel 5x5 @ dilation 2 becomes 9x9, 12-9+1 = 4
 	nn.Flatten(),
 	# flatten preserves the 0th dim, so must keep batch, that's why earlier layers view tuple includes it
 	nn.Linear(256, 10),  # 4x4x16=256
-	snn.Readout(10, beta),
+	snn.Readout(10, model_layers_beta),
 	nn.Softmax(-1),
 ).to(device)
 
@@ -112,9 +134,6 @@ print(f"Total parameters: {total_params:,}")
 loss_fn = tt.loss.soft_cross_entropy
 # loss_fn = nn.functional.mse_loss
 
-num_epochs = 1
-num_timesteps = 20
-ema_decay = tt.functional.halflife_to_decay(num_timesteps / 2)
 
 train_types = ["bptt_dense", "bptt_sparse", "online_dense", "online_sparse"]
 
@@ -123,15 +142,12 @@ for tr_type in train_types:
 
 	optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
-	train_losses, test_losses = [], []
-	train_accuracies, test_accuracies = [], []
-	loss_manager = tt.plot.MeasurementManager(f"Loss - {tr_type}", [0.0, 0.9, 0.99])
-	accuracy_manager = tt.plot.MeasurementManager(f"Accuracy - {tr_type}", [0.0, 0.9, 0.99])
+	loss_manager = tt.plot.MeasurementManager(f"{tr_type} - Loss", [0.0, 0.9, 0.99])
+	accuracy_manager = tt.plot.MeasurementManager(f"{tr_type} - Accuracy", [0.0, 0.9, 0.99])
 
+	# TRAIN
+	model.train()
 	for e in range(num_epochs):
-		# TRAIN
-		model.train()
-		temp_loss, temp_accuracy = 0, 0
 		for (image, label) in tqdm(train_dloader, total=len(train_dloader), desc=f"{tr_type} - E{e + 1} - TRAIN"):
 			b, c, h, w = image.shape
 			image, label = image.to(device), label.to(device)
@@ -149,7 +165,6 @@ for tr_type in train_types:
 
 				recording_loss = running_loss.item()
 				running_loss.backward()
-				optimizer.step()
 
 			if tr_type == "bptt_sparse":
 				running_loss = torch.tensor(0, device=device)
@@ -161,7 +176,6 @@ for tr_type in train_types:
 
 				recording_loss = running_loss.item()
 				running_loss.backward()
-				optimizer.step()
 
 			if tr_type == "online_dense":
 				for t in range(num_timesteps):
@@ -173,10 +187,9 @@ for tr_type in train_types:
 					spike_input = torch.bernoulli(image)
 					model_output = model(spike_input)
 					loss = loss_fn(model_output, label) * (1 - ema_decay)
+					recording_loss = loss.item()
 					loss.backward()
 					model.detach_states()
-
-				recording_loss = loss.item()
 
 			if tr_type == "online_sparse":
 				for t in range(num_timesteps):
@@ -188,108 +201,78 @@ for tr_type in train_types:
 					spike_input = torch.bernoulli(image)
 					model_output = model(spike_input)
 					loss = loss_fn(model_output, label)
+					recording_loss = loss.item()
 					loss.backward()
 					model.detach_states()
 
-				recording_loss = loss.item()
-
 			optimizer.step()
-
 			with torch.no_grad():
 				loss_manager.append(recording_loss)
-				temp_loss += recording_loss
 				pred_classes = model_output.argmax(dim=1)
 				true_classes = label.argmax(dim=1)
-				num_correct = (pred_classes == true_classes).sum().item()
-				temp_accuracy += num_correct
-				num_correct /= b
+				num_correct = (pred_classes == true_classes).sum().item() / b
 				accuracy_manager.append(num_correct)
 
-		temp_loss /= len(train_dloader)
-		temp_accuracy /= 60_000
-		train_losses.append(temp_loss)
-		train_accuracies.append(temp_accuracy)
+	loss_manager.plot()
+	accuracy_manager.plot()
 
-		loss_manager.plot()
-		accuracy_manager.plot()
+	# TEST
+	model.eval()
+	test_loss, test_accuracy = 0, 0
+	with torch.no_grad():
+		for (image, label) in tqdm(test_dloader, total=len(test_dloader), desc=f"{tr_type} - TEST"):
+			b, c, h, w = image.shape
+			image, label = image.to(device), label.to(device)
 
-		# TEST
-		model.eval()
-		temp_loss, temp_accuracy = 0, 0
-		with torch.no_grad():
-			for (image, label) in tqdm(test_dloader, total=len(test_dloader), desc=f"{tr_type} - E{e + 1} - TEST"):
-				b, c, h, w = image.shape
-				image, label = image.to(device), label.to(device)
+			model.zero_states()
+			model.zero_grad()
 
-				model.zero_states()
-				model.zero_grad()
-
-				for t in range(num_timesteps):
-					spike_input = torch.bernoulli(image)
-					model_output = model(spike_input)
-					loss = loss_fn(model_output, label)
-
-				temp_loss += loss.item()
-				true_classes = label.argmax(dim=1)
-				pred_classes = model_output.argmax(dim=1)
-				correct = (pred_classes == true_classes).sum().item()
-				temp_accuracy += correct
-
-			temp_loss /= len(test_dloader)
-			temp_accuracy /= 10_000
-			test_losses.append(temp_loss)
-			test_accuracies.append(temp_accuracy)
-
-		# PLOT
-		print(f"Train - Loss: {train_losses[-1]:.5f}, Accuracy: {train_accuracies[-1] * 100:.5f}%")
-		print(f"Test - Loss: {test_losses[-1]:.5f}, Accuracy: {test_accuracies[-1] * 100:.5f}%")
-
-		if len(train_losses) > 1:
-			plt.title(f"LOSS - {tr_type}")
-			plt.plot(train_losses, label="Train")
-			plt.plot(test_losses, label="Test")
-			plt.legend()
-			plt.show()
-
-		if len(train_accuracies) > 1:
-			plt.title(f"ACCURACY - {tr_type}")
-			plt.plot(train_accuracies, label="Train")
-			plt.plot(test_accuracies, label="Test")
-			plt.legend()
-			plt.show()
-
-		# model_betas = model.get_attr_list("beta", "beta_scalar")
-		# for t in model_betas:
-		# 	print(f"{nn.functional.sigmoid(t)}")
-		# model_thresholds = model.get_attr_list("threshold")
-		# for t in model_thresholds:
-		# 	print(f"{nn.functional.softplus(t)}")
-
-		# VISUALIZE
-		with torch.no_grad():
-			images, labels = next(iter(test_dloader))
-			images, labels = images.to(device), labels.to(device)
-
-			for i, (image, label) in tqdm(enumerate(zip(images, labels)), total=len(images),
-										  desc=f"{tr_type} - E{e + 1} - VISUALIZE"):
-				if i >= num_to_visualize:
-					continue
-
-				empty_image = torch.zeros_like(image)
-
-				spike_train = []
-				model.zero_states()
-
-				for t in range(num_timesteps):
-					spike_input = torch.bernoulli(image)
-					model_output = model(spike_input.unsqueeze(0)).squeeze()
-					empty_image += spike_input
-					spike_train.append(model_output.squeeze())
-
-				empty_image /= num_timesteps
+			for t in range(num_timesteps):
+				spike_input = torch.bernoulli(image)
+				model_output = model(spike_input)
 				loss = loss_fn(model_output, label)
-				real_number = torch.argmax(label).item()
-				pred_number = torch.argmax(model_output).item()
-				title = f"{tr_type} - Was: {real_number}, pred: {pred_number}, loss: {loss.item():.5f}"
-				tt.plot.render_image(empty_image, title=title)
-				tt.plot.spike_train(spike_train, title=title)
+
+			test_loss += loss.item()
+			true_classes = label.argmax(dim=1)
+			pred_classes = model_output.argmax(dim=1)
+			correct = (pred_classes == true_classes).sum().item()
+			test_accuracy += correct
+
+		test_loss /= len(test_dloader)
+		test_accuracy /= len(test_dloader.dataset)
+
+	# PRINT
+	print(f"Test - Loss: {test_loss:.5f}, Accuracy: {test_accuracy * 100:.5f}%")
+
+	model_betas = model.get_attr_list("beta", "beta_scalar")
+	for t in model_betas:
+		beta = nn.functional.sigmoid(t)
+		print(f"Beta - mean: {beta.mean()}, std: {beta.std() if beta.numel() > 1 else None}")
+
+	# VISUALIZE
+	with torch.no_grad():
+		images, labels = next(iter(test_dloader))
+		images, labels = images.to(device), labels.to(device)
+
+		for i, (image, label) in tqdm(enumerate(zip(images, labels)), total=len(images), desc=f"{tr_type} - VISUALIZE"):
+			if i >= num_to_visualize:
+				continue
+
+			empty_image = torch.zeros_like(image)
+
+			spike_train = []
+			model.zero_states()
+
+			for t in range(num_timesteps):
+				spike_input = torch.bernoulli(image)
+				model_output = model(spike_input.unsqueeze(0)).squeeze()
+				empty_image += spike_input
+				spike_train.append(model_output.squeeze())
+
+			empty_image /= num_timesteps
+			loss = loss_fn(model_output, label)
+			real_number = torch.argmax(label).item()
+			pred_number = torch.argmax(model_output).item()
+			title = f"{tr_type} - Was: {real_number}, pred: {pred_number}, loss: {loss.item():.5f}"
+			tt.plot.render_image(empty_image, title=title)
+			tt.plot.spike_train(spike_train, title=title)
