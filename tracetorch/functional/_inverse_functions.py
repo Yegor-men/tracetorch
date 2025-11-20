d61fa68d67cd366eef8bc4c6f00897aa2737f8d5
Yegor Men <yegormenovchshikov@gmail.com>
2025-11-20 18:38:01 +1100

    Added assertion for the values are in the proper range

--- tracetorch/functional/_inverse_functions.py
+++ tracetorch/functional/_inverse_functions.py
@@ -2,8 +2,10 @@ import torch
 
 
 def sigmoid_inverse(x: torch.Tensor) -> torch.Tensor:
+	assert torch.all((x > 0) & (x < 1)), "Tensor for inverse sigmoid has values outside the (0,1) range"
 	return torch.logit(x)
 
 
 def softplus_inverse(x: torch.Tensor) -> torch.Tensor:
+	assert torch.all(x > 0), "Tensor for softplus inverse has values outside the (0,inf) range"
 	return torch.log(torch.expm1(x))
