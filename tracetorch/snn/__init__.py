f065aa3d474d432e7ef773dbd17c5147b4374d61
Yegor-men <yegor.mn@gmail.com>
2026-02-14 16:46:18 +1100

    Added DLI

--- tracetorch/snn/__init__.py
+++ tracetorch/snn/__init__.py
@@ -1,9 +1,13 @@
 from ._ttmodule import TTModule
 from ._param_setup import SetupMixin
 
-from ._li_layers import LI, DLI, SLI, RLI, DSLI, DRLI, SRLI, DSRLI
-from ._lib_layers import LIB, DLIB, SLIB, RLIB, DSLIB, DRLIB, SRLIB, DSRLIB
-from ._lit_layers import LIT, DLIT, SLIT, RLIT, DSLIT, DRLIT, SRLIT, DSRLIT
-from ._lits_layers import LITS, DLITS, SLITS, RLITS, DSLITS, DRLITS, SRLITS, DSRLITS
+# from ._li_layers import LI, DLI, SLI, RLI, DSLI, DRLI, SRLI, DSRLI
+# from ._lib_layers import LIB, DLIB, SLIB, RLIB, DSLIB, DRLIB, SRLIB, DSRLIB
+# from ._lit_layers import LIT, DLIT, SLIT, RLIT, DSLIT, DRLIT, SRLIT, DSRLIT
+# from ._lits_layers import LITS, DLITS, SLITS, RLITS, DSLITS, DRLITS, SRLITS, DSRLITS
 
 # import flex
+
+from ._li_layers import LI, DLI, SLI
+from ._lib_layers import LIB, DLIB, SLIB, RLIB
+from ._lits_layers import LITS
--- tracetorch/snn/_li_layers.py
+++ tracetorch/snn/_li_layers.py
@@ -44,6 +44,67 @@ class LI(TTModule, SetupMixin):
         return self.mem
 
 
+class DLI(TTModule, SetupMixin):
+    def __init__(
+            self,
+            num_neurons: int,
+            pos_beta: Union[float, torch.Tensor] = 0.9,
+            neg_beta: Union[float, torch.Tensor] = 0.9,
+            dim: int = -1,
+            pos_beta_rank: Literal[0, 1] = 1,
+            neg_beta_rank: Literal[0, 1] = 1,
+            learn_pos_beta: bool = True,
+            learn_neg_beta: bool = True,
+    ):
+        super().__init__()
+        self.num_neurons = int(num_neurons)
+        self.dim = int(dim)
+
+        self.pos_mem = None
+        self.neg_mem = None
+        self._register_decay("pos_beta", pos_beta, pos_beta_rank, learn_pos_beta)
+        self._register_decay("neg_beta", neg_beta, neg_beta_rank, learn_neg_beta)
+
+    @property
+    def pos_beta(self):
+        return nn.functional.sigmoid(self.raw_pos_beta)
+
+    @property
+    def neg_beta(self):
+        return nn.functional.sigmoid(self.raw_neg_beta)
+
+    def zero_states(self):
+        self.pos_mem = None
+        self.neg_mem = None
+
+    def detach_states(self):
+        if self.pos_mem is not None:
+            self.pos_mem = self.pos_mem.detach()
+        if self.neg_mem is not None:
+            self.neg_mem = self.neg_mem.detach()
+
+    def forward(self, x):
+        if self.pos_mem is None:
+            self.pos_mem = torch.zeros_like(x)
+        if self.neg_mem is None:
+            self.neg_mem = torch.zeros_like(x)
+
+        x_moved = x.movedim(self.dim, -1)
+
+        pos_mem_moved = self.pos_mem.movedim(self.dim, -1)
+        neg_mem_moved = self.neg_mem.movedim(self.dim, -1)
+
+        pos_mem_moved = pos_mem_moved * self.pos_beta + torch.where(x_moved >= 0, x_moved, 0.0)
+        neg_mem_moved = neg_mem_moved * self.neg_beta + torch.where(x_moved < 0, x_moved, 0.0)
+
+        mem_moved = pos_mem_moved + neg_mem_moved
+        mem = mem_moved.movedim(-1, self.dim)
+
+        self.pos_mem = pos_mem_moved.movedim(-1, self.dim)
+        self.neg_mem = neg_mem_moved.movedim(-1, self.dim)
+        return mem
+
+
 class SLI(TTModule, SetupMixin):
     def __init__(
             self,
