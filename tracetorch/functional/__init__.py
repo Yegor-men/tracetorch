3016df4df8be5e8b142c73d31eedd4dff6c72669
Yegor-men <yegor.mn@gmail.com>
2026-04-08 03:05:51 +1000

    Added hippo decay stuff

--- tracetorch/functional/__init__.py
+++ tracetorch/functional/__init__.py
@@ -1,4 +1,5 @@
-from ._decay_conversion import halflife_to_decay, decay_to_halflife, timesteps_to_decay, decay_to_timesteps
+from ._decay_calculations import halflife_to_decay, decay_to_halflife, timesteps_to_decay, decay_to_timesteps, \
+    hippo_decays
 from ._inverse_functions import sigmoid_inverse, softplus_inverse
 from ._spike_functions import sigmoid4x
 from ._surrogates import round_ste, bernoulli_ste, probabilistic_ste
---
+++ tracetorch/functional/_decay_calculations.py
@@ -0,0 +1,33 @@
+import math
+import torch
+
+
+def halflife_to_decay(num_steps: float):
+    return math.exp(-math.log(2) / num_steps)
+
+
+def decay_to_halflife(decay: float):
+    return -math.log(2) / math.log(decay)
+
+
+def timesteps_to_decay(time: float):
+    return 1 - 1 / time
+
+
+def decay_to_timesteps(decay: float):
+    return 1 / (1 - decay)
+
+
+def hippo_decays(N: int, scale: float = 0.01, device=None, dtype=torch.float32, shuffle: bool = False):
+    """
+    Returns a vector of N values with exactly the HiPPO diagonal spectrum
+
+    Slowest decay = exp(-1 * scale) -> long memory
+    Fastest decay = exp(-N * scale) -> short memory
+    """
+    rates = torch.arange(1, N + 1, dtype=dtype, device=device)
+    decays = torch.exp(-rates * scale)
+    if shuffle:
+        idx = torch.randperm(N, device=device)
+        decays = decays[idx]
+    return decays
