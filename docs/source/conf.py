93a318703af5ab1df3541e5027c14698d7e1b0a4
Yegor-men <yegor.mn@gmail.com>
2026-04-13 20:13:01 +1000

    version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'traceTorch'
 copyright = '2026, Yegor Menovchshikov'
 author = 'Yegor Menovchshikov'
-release = 'v0.16.3'
+release = 'v0.16.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
