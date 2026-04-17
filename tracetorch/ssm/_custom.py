d5c6c554d434973e6d04b3906748dd4fd5b2871f
Yegor-men <yegor.mn@gmail.com>
2026-04-17 21:32:33 +1000

    fixed some stuff, still a bit garbage though

--- tracetorch/ssm/_custom.py
+++ tracetorch/ssm/_custom.py
@@ -5,64 +5,88 @@ from typing import Union, Literal
 
 
 class SelectiveSSM(SSMLayer):
-    def __init__(self, num_neurons: int, d_state: int = 16, decay: Union[float, torch.Tensor] = 0.9,
-                 decay_rank: Literal[0, 1] = 1, learn_decay: bool = True, dim: int = -1):
+    def __init__(
+            self,
+            num_neurons: int,
+            d_state: int = 16,
+            decay: Union[float, torch.Tensor] = 0.9,
+            dim: int = -1,
+            decay_rank: Literal[0, 1] = 1,
+            learn_decay: bool = True,
+    ):
         super().__init__(num_neurons, dim, d_state=d_state)
 
         self._register_state_scale("scale", decay, decay_rank, learn_decay)
 
-        self.A_proj = nn.Linear(num_neurons, d_state)
-        self.B_proj = nn.Linear(num_neurons, num_neurons)
-        self.C = nn.Parameter(torch.randn(d_state))
+        self.ABD = nn.Linear(num_neurons, d_state + num_neurons + num_neurons)
+        nn.init.zeros_(self.ABD.bias)
+
+        self.C = nn.Linear(d_state, 1)
+        nn.init.zeros_(self.C.bias)
+
         self._initialize_state("state")
 
     def forward(self, x):
         self._ensure_states(x)
         x_w = self._to_working_dim(x)
 
-        delta = nn.functional.softplus(self.A_proj(x_w))  # [..., N]
-        decay = torch.exp(delta * -self.scale).unsqueeze(-2)  # [..., 1, N]
-        b_x = self.B_proj(x_w)  # [..., D]
+        A, B, D = torch.split(self.ABD(x_w), [self.d_state, self.num_neurons, self.num_neurons], dim=-1)
+        # A, B, D are [..., D], [..., N], [..., D]
 
-        state = self._state_to_working_dim(self.state)  # [..., D, N]
-        state = state * decay + b_x.unsqueeze(-1) * (1 - decay)
+        delta = nn.functional.softplus(A)  # [..., D]
+        decay = torch.exp(delta * -self.scale).unsqueeze(-2)  # [..., 1, D]
+
+        state = self._state_to_working_dim(self.state)  # [..., N, D]
+        state = state * decay + B.unsqueeze(-1) * (1 - decay)
         self.state = self._state_from_working_dim(state)
 
-        y = torch.sum(state * self.C, dim=-1)
-        return self._from_working_dim(y)
+        out = x_w + self.C(state).squeeze(-1) * nn.functional.silu(D)
+        out = self._from_working_dim(out)
+
+        return out
 
 
 class SelectiveZOHSSM(SSMLayer):
-    def __init__(self, num_neurons: int, d_state: int = 16, decay: Union[float, torch.Tensor] = 0.9,
-                 decay_rank: Literal[0, 1] = 1, learn_decay: bool = True, dim: int = -1):
+    def __init__(
+            self,
+            num_neurons: int,
+            d_state: int = 16,
+            dim: int = -1,
+            learn_decay: bool = True
+    ):
         super().__init__(num_neurons, dim, d_state=d_state)
 
-        self._register_log_state_scale("scale", decay, decay_rank, learn_decay)
+        self._register_log_state_scale("scale", torch.arange(1, d_state + 1), 1, learn_decay)
 
-        self.A_proj = nn.Linear(num_neurons, d_state)
-        self.B_proj = nn.Linear(num_neurons, num_neurons)
-        self.C = nn.Parameter(torch.randn(d_state))
+        self.ABD = nn.Linear(num_neurons, d_state + num_neurons + num_neurons)
+        nn.init.zeros_(self.ABD.bias)
+
+        self.C = nn.Linear(d_state, 1)
+        nn.init.zeros_(self.C.bias)
+
         self._initialize_state("state")
 
     def forward(self, x):
         self._ensure_states(x)
         x_w = self._to_working_dim(x)
 
-        delta = nn.functional.softplus(self.A_proj(x_w))
-        A_eff = -self.scale
+        A, B, D = torch.split(self.ABD(x_w), [self.d_state, self.num_neurons, self.num_neurons], dim=-1)
+        # A, B, D are [..., D], [..., N], [..., D]
 
-        delta_A = delta * A_eff
-        bar_A = torch.exp(delta_A).unsqueeze(-2)  # [..., 1, N]
-        bar_B = ((torch.exp(delta_A) - 1) / (A_eff + 1e-12) * delta).unsqueeze(-2)  # [..., 1, N]
+        delta = nn.functional.softplus(A)
+        delta_A = delta * -self.scale
 
-        b_x = self.B_proj(x_w)
+        bar_A = torch.exp(delta_A).unsqueeze(-2)  # [..., 1, D]
+        bar_B = ((torch.exp(delta_A) - 1) / (-self.scale + 1e-12) * delta).unsqueeze(-2)  # [..., 1, D]
 
         state = self._state_to_working_dim(self.state)
-        state = state * bar_A + bar_B * b_x.unsqueeze(-1)
+        state = state * bar_A + bar_B * B.unsqueeze(-1)
         self.state = self._state_from_working_dim(state)
 
-        y = torch.sum(state * self.C, dim=-1)
-        return self._from_working_dim(y)
+        out = x_w + self.C(state).squeeze(-1) * nn.functional.silu(D)
+        out = self._from_working_dim(out)
+
+        return out
 
 
 class SpikeSSM(SSMLayer):
